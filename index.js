71db8444d0e0c64280fe863b562bbb4cee0f327d
L C <LF9904@gmail.com>
2025-01-18 20:22:17 -0500

    added npm caporl, lodash debounce, & chokidar

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 #!/usr/bin/env node
-console.log("I got executed");
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const program = require("caporal");
+const fs = require("fs");
+const {spawn} = require("child_process");
+
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of a file to execute")
+  .action(async ({filename}) => {
+    const name = filename || "index.js";
+    try {
+      await fs.promises.access(name);
+    } catch (e) {
+      throw new Error(`File ${name} does not exist`);
+    }
+
+    const start = debounce(() => {
+      spawn("node", [name], {stdio: "inherit"});
+    }, 100);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
